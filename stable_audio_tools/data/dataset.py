d314044429bcce3fcdf7506dadbf882239047cc5
ChiragPritmanii <cpritmani@gmail.com>
2024-05-23 01:52:25 +0530

    Debugging

--- stable_audio_tools/data/dataset.py
+++ stable_audio_tools/data/dataset.py
@@ -21,6 +21,8 @@ from typing import Optional, Callable, List
 
 from .utils import Stereo, Mono, PhaseFlipper, PadCrop_Normalized_T
 
+torchaudio.set_audio_backend("sox_io")
+
 AUDIO_KEYS = ("flac", "wav", "mp3", "m4a", "ogg", "opus")
 
 # fast_scandir implementation by Scott Hawley originally in https://github.com/zqevans/audio-diffusion/blob/main/dataset/dataset.py
