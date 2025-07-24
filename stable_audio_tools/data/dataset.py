4039e30fc6643104edbf0d1c3e9a880b62f0e4c5
ChiragPritmanii <cpritmani@gmail.com>
2025-07-25 01:56:30 +0530

    debug wds

--- stable_audio_tools/data/dataset.py
+++ stable_audio_tools/data/dataset.py
@@ -705,7 +705,7 @@ class WebDatasetDataLoader():
         self.data_loader = wds.WebLoader(self.dataset, num_workers=num_workers, worker_init_fn=worker_init_fn, **data_loader_kwargs)
 
     def wds_preprocess(self, sample):
-        print(sample["json"])
+        print(sample.keys())
 
         if self.pre_encoded:
             audio = torch.from_numpy(sample["npy"])
