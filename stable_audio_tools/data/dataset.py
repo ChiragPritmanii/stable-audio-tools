6a82d48855508df38e60bba405c8574111f8a75e
ChiragPritmanii <cpritmani@gmail.com>
2025-07-25 03:12:51 +0530

    debug wds

--- stable_audio_tools/data/dataset.py
+++ stable_audio_tools/data/dataset.py
@@ -721,7 +721,8 @@ class WebDatasetDataLoader():
                 break
         if '' == found_key:  # got no audio!
             return None  # try returning None to tell WebDataset to skip this one
-
+        
+        print(f"Found audio key: {found_key} in sample {sample['json']['__url__']}")
         audio, in_sr = sample[found_key]
         if in_sr != self.sample_rate:
             resample_tf = T.Resample(in_sr, self.sample_rate)
